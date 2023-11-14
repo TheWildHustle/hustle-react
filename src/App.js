a3162a0a6d8d2dd94cf254185af61b5ee97cef15
TheWildHustle <TheWildHustle@proton.me>
2023-11-14 15:32:56 -0500

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [price, setPrice] = useState(0);
+    const getPrice = () => {
+      axios 
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((res) => {
+        setPrice(res.data.bpi.USD.rate_float);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }; 
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bitcoin Price</h1>
+      <h3>${price}</h3>
     </div>
   );
 }
